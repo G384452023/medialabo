298ebfd673c95ee6e0f92398cc3b80d341cb93d3
G384452023 <238445@st.tskushoku-u.ac.jp>
2024-05-30 17:13:06 +0900

    kazuate.js

--- kazuate.js
+++ kazuate.js
@@ -8,27 +8,27 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-
+let b = document.querySelector('#print');
+b.addEventListener('click', hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
-  if(kaisu < 4){
-    kaisu++;
-    let p = document.querySelector('span#kaisu');
+kaisu++;
+  let i = document.querySelector('input[name="kazuate"]');
+  let yoso = i.value;  
+  let p = document.querySelector('span#kaisu');
     let p1 = document.createElement('p');  
     p.textContent = kaisu;
     let p2 = document.querySelector('span#answer');
-    let p3 = document.createElement('p2'); 
+    let p3 = document.createElement('p'); 
     p2.textContent = yoso;
-    console.log(kaisu + "回目の予想: " + yoso);
     let p4 = document.querySelector('p#result');
     let p5 = document.createElement('p4');
-    p4.textContent = '答えは ' + kotae + 'でした';
-    if(kotae === yoso){
-        if(kaisu === 1){
+  if(kaisu < 4){
+    console.log(kaisu + "回目の予想: " + yoso);
+    if(kotae == yoso){
+        if(kaisu < 4){
             console.log("正解！！！");
             p4.textContent = '正解！！！';
         }
@@ -49,7 +49,6 @@ function hantei() {
     }
 }
 else{
-    kotae++;
     console.log(kaisu + "回目の予想: " + yoso);
     console.log("答えは" + kotae + "でした、すでにゲームは終わっています。");
     p4.textContent = '答えは' + kotae + 'でした、すでにゲームは終わっています。';
